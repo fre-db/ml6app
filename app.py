ff781c7a5c94afe5b4bfd84b25d35ee7d6780cb1
Frédérique De Baerdemaeker <frederique@helloprof.com>
2019-11-30 21:08:02 +0100

    introduce api docs

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 import os
-from flask import Flask, jsonify, request, render_template
+from flask import Flask, jsonify, request, render_template, Blueprint
+from flask_restplus import Api, reqparse, Resource, abort
 
 from google.cloud import speech
 from google.cloud.speech import types
 from google.protobuf.json_format import MessageToJson
+from werkzeug.datastructures import FileStorage
 
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "ML6 Application-0e9fc2df08a1.json"
 
 app = Flask(__name__, static_url_path='/static')
+blueprint = Blueprint('api', __name__, url_prefix='/api')
+api = Api(blueprint, default='api')
+app.register_blueprint(blueprint)
 
 
 @app.context_processor
@@ -19,18 +24,6 @@ def vars():
 
 client = speech.SpeechClient()
 debug = False
-debug_resp = """
-{"results": [
-        {
-            "alternatives": [
-                {
-                    "transcript": "oh yeah Vine",
-                    "confidence": 0.8270418047904968
-                }
-            ]
-        }
-    ]}
-"""
 
 
 @app.route('/')
@@ -38,46 +31,52 @@ def index():
     return render_template('upload.html')
 
 
-@app.route('/upload', methods=['POST'])
-def process_wav():
-    file = request.files.get('file', None)
-    if not file:
-        raise InvalidUsage('No file provided')
+parser = reqparse.RequestParser()
+parser.add_argument('file', type=FileStorage, location='files', required=True, help='.wav file to transcribe')
+parser.add_argument('language', type=str, location='form', help='language code, e.g. en-US')
 
-    if debug:
-        return jsonify(debug_resp)
 
-    lang = request.form.get('language', 'en-US')
-    config = types.RecognitionConfig(language_code=lang, audio_channel_count=2)
-    try:
-        response = client.recognize(config, types.RecognitionAudio(content=file.read()))
+@api.route('/upload/', methods=['POST'])
+@api.expect(parser, validate=True)
+class Upload(Resource):
+    debug_resp = """
+    {"results": [
+            {
+                "alternatives": [
+                    {
+                        "transcript": "oh yeah Vine",
+                        "confidence": 0.8270418047904968
+                    }
+                ]
+            }
+        ]}
+    """
+
+    def transcribe_file(self, file, lang):
+        config = types.RecognitionConfig(language_code=lang, audio_channel_count=2)
+        audio = types.RecognitionAudio(content=file.read())
+        response = client.recognize(config, audio)
         return jsonify(MessageToJson(response))
-    except Exception as e:
-        raise InvalidUsage(str(e))
+
+    @api.response(200, 'JSON Response returned by Google Speech to Text API')
+    def post(self):
+        file = request.files.get('file', None)
+
+        if not file:
+            abort(400, 'No file provided')
+
+        if debug:
+            return jsonify(self.debug_resp)
+
+        lang = request.form.get('language', 'en-US')
+
+        return self.transcribe_file(file, lang)
 
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
 
 
-class InvalidUsage(Exception):
-    status_code = 400
-
-    def __init__(self, message, status_code=None, payload=None):
-        Exception.__init__(self)
-        self.message = message
-        if status_code is not None:
-            self.status_code = status_code
-        self.payload = payload
-
-    def to_dict(self):
-        rv = dict(self.payload or ())
-        rv['message'] = self.message
-        return rv
-
-
-@app.errorhandler(InvalidUsage)
+@api.errorhandler(Exception)
 def handle_invalid_usage(error):
-    response = jsonify(error.to_dict())
-    response.status_code = error.status_code
-    return response
+    return {'message': str(error)}, getattr(error, 'code', 500)
--- static/webapp.js
+++ static/webapp.js
@@ -1,47 +1,56 @@
+var result_div = null, fileInput = null, langInput = null;
+
 $(document).ready(function () {
-  const result_div = $('.results');
-  const fileInput = $('input[name="file"]');
-  const langInput = $('input[name="language"]');
+  result_div = $('.results');
+  fileInput = $('input[name="files"]');
+  langInput = $('input[name="language"]');
 
   $('form').submit(function (e) {
     e.preventDefault();
     if (!fileInput.val()) return;
 
-    const new_result = $("#template").clone();
-    new_result.removeAttr('id');
-    new_result.find("#filename").text(fileInput.val().split('\\').pop());
-    const lang_label = document.querySelector("#languages option[value='"+langInput.val()+"']").text;
-    new_result.find("#result-lang").text(lang_label);
-    result_div.prepend(new_result);
-    new_result.show();
-    result_div.show();
-
-    const formData = new FormData(this);
-
-    $.ajax({
-      url: "/upload",
-      type: "POST",
-      data: formData,
-      processData: false,
-      contentType: false,
-      success: function (response) {
-        const response_obj = JSON.parse(response);
-        let recognized_text = "";
-        let confidence = 0;
-        response_obj.results.forEach(function (result) {
-          recognized_text += result.alternatives[0].transcript;
-          confidence += result.alternatives[0].confidence;
-        });
-        new_result.find(".loader").replaceWith('<p>' + recognized_text + '</p>');
-        new_result.find("progress").val(confidence / response_obj.results.length);
-        jsonTree.create(response_obj, new_result.find('#response')[0]);
-        new_result.find(".success").show();
-      },
-      error: function (xhr, status, error) {
-        const header = new_result.find("h2");
-        header.text(header.text().replace('Transcription for', 'Error for'))
-        new_result.find(".loader").replaceWith('<p class="error">' + xhr.responseText + '</p>')
-      }
-    });
+    for (file of fileInput[0].files) {
+      transcribe_file(this, file);
+    }
   });
 });
+
+function transcribe_file(form, file) {
+  const new_result = $("#template").clone();
+  new_result.removeAttr('id');
+  new_result.find("#filename").text(file.name);
+  const lang_label = document.querySelector("#languages option[value='" + langInput.val() + "']").text;
+  new_result.find("#result-lang").text(lang_label);
+  result_div.prepend(new_result);
+  new_result.show();
+  result_div.show();
+
+  const formData = new FormData(form);
+  formData.append('file', file);
+
+  $.ajax({
+    url: "/api/upload/",
+    type: "POST",
+    data: formData,
+    processData: false,
+    contentType: false,
+    success: function (response) {
+      const response_obj = JSON.parse(response);
+      let recognized_text = "";
+      let confidence = 0;
+      response_obj.results.forEach(function (result) {
+        recognized_text += result.alternatives[0].transcript;
+        confidence += result.alternatives[0].confidence;
+      });
+      new_result.find(".loader").replaceWith('<p>' + recognized_text + '</p>');
+      new_result.find("progress").val(confidence / response_obj.results.length);
+      jsonTree.create(response_obj, new_result.find('#response')[0]);
+      new_result.find(".success").show();
+    },
+    error: function (xhr, status, error) {
+      const header = new_result.find("h2");
+      header.text(header.text().replace('Transcription for', 'Error for'))
+      new_result.find(".loader").replaceWith('<p class="error">' + xhr.responseText + '</p>')
+    }
+  });
+}
